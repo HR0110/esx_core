4942da9ef1b52fa36bd2683ea276734514c81c0b
HR0110 <hrisiraya@abv.bg>
2025-10-09 14:48:58 +0300

    fix(fxmanifest): returning the fxmanifest to its initial look

--- [core]/es_extended/fxmanifest.lua
+++ [core]/es_extended/fxmanifest.lua
@@ -7,13 +7,14 @@ version '1.13.4'
 
 shared_scripts {
 	'locale.lua',
-    'shared/config/main.lua',
-    'shared/config/adjustments.lua',
-    'shared/config/logs.lua',
+
+	'shared/config/main.lua',
     'shared/config/weapons.lua',
+    'shared/config/adjustments.lua',
+
     'shared/main.lua',
-	'shared/modules/*',
     'shared/functions.lua',
+    'shared/modules/*.lua',
 }
 
 server_scripts {
@@ -22,19 +23,21 @@ server_scripts {
 
 	'server/common.lua',
 	'server/modules/callback.lua',
-	'server/classes/*.*',
-	'server/classes/xPlayer/main.lua',
+	'server/classes/player.lua',
+	'server/classes/vehicle.lua',
+	'server/classes/overrides/*.lua',
 	'server/functions.lua',
 	'server/modules/onesync.lua',
 	'server/modules/paycheck.lua',
-	'server/modules/getXPlayer.lua',
 
 	'server/main.lua',
 	'server/modules/commands.lua',
 
 	'server/bridge/**/*.lua',
 	'server/modules/npwd.lua',
-	'server/modules/createJob.lua'
+	'server/modules/createJob.lua',
+	'server/migration/**/main.lua',
+	'server/migration/main.lua',
 }
 
 client_scripts {
@@ -64,8 +67,16 @@ files {
 	'locales/*.lua',
 	'locale.js',
 	'html/ui.html',
-	'html/**/*',
-    'client/imports/*.lua',
+
+	'html/css/app.css',
+
+	'html/js/mustache.min.js',
+	'html/js/wrapper.js',
+	'html/js/app.js',
+
+	'html/fonts/pdown.ttf',
+	'html/fonts/bankgothic.ttf',
+    "client/imports/*.lua",
 }
 
 dependencies {
